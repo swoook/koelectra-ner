829243af4a23aa46d1cbc015abd39424d76d185e
swoook <kimseongwoo21@gmail.com>
2023-01-31 10:13:21 +0000

    ✨ feature(src.data.aihub_pfc): Implemented `create_word_label_sequence`
    - Renamed `create_label_sequence` as `create_word_label_sequence`
    - Need to handle marks or signs such as . ! ? , + - ...

--- src/data/aihub_pfc/transformation.py
+++ src/data/aihub_pfc/transformation.py
@@ -1,49 +1,62 @@
 from copy import deepcopy
 
 
-def create_label_sequence(example):
-    result = list()
+def create_positive_subsequence(substring, label):
+    words = substring.split()
+    labels = [''.join([label, '_B'])]
+    for idx in range(len(words) - 1): labels.append(''.join([label, '_I']))
+    return words, labels
+
+def create_negative_subsequence(substring, label):
+    words = substring.split()
+    labels = ['-' for _ in range(len(words))]
+    return words, labels
+
+def create_subsequence(substring, label):
+    if label == '-': return create_negative_subsequence(substring, label)
+    else: return create_positive_subsequence(substring, label)
+
+def create_word_label_sequence(example):
+    words = list()
+    labels = list()
     
     entities_sorted = sorted(example['NE'], key=lambda item: item['begin'])
     
     if entities_sorted[0]['begin'] > 0:
         tail_idx = entities_sorted[0]['begin']
-        labels.append(
-            {
-                'id': None, 
-                'entity': example['text'][:tail_idx], 
-                'type': 'O', 
-                'begin': 0, 
-                'end': tail_idx,
-            }
-        )
+        substring = example['text'][:tail_idx]
+        subsequence = create_subsequence(substring, '-')
+        words.extend(subsequence[0])
+        labels.extend(subsequence[1])
         
     for idx, entity in enumerate(entities_sorted[:-1]):
-        labels.append(deepcopy(entity))
+        head_idx = entity['begin']
+        tail_idx = entity['end']
+        substring = example['text'][head_idx:tail_idx]
+        subsequence = create_subsequence(substring, entity['type'])
+        words.extend(subsequence[0])
+        labels.extend(subsequence[1])
+        
         head_idx = entities_sorted[idx]['end']
         tail_idx = entities_sorted[idx+1]['begin']
-        labels.append(
-            {
-                'id': None, 
-                'entity': example['text'][head_idx:tail_idx], 
-                'type': 'O', 
-                'begin': head_idx, 
-                'end': tail_idx,
-            }
-        )
-        
-    labels.append(deepcopy(entities_sorted[-1]))
+        substring = example['text'][head_idx:tail_idx]
+        subsequence = create_subsequence(substring, '-')
+        words.extend(subsequence[0])
+        labels.extend(subsequence[1])
+    
+    head_idx = entities_sorted[-1]['begin']
+    tail_idx = entities_sorted[-1]['end']
+    substring = example['text'][head_idx:tail_idx]
+    subsequence = create_subsequence(substring, entities_sorted[-1]['type'])
+    words.extend(subsequence[0])
+    labels.extend(subsequence[1])
+    
     if entities_sorted[-1]['end'] < len(example['text']):
         head_idx = entities_sorted[-1]['end']
         tail_idx = len(example['text'])
-        labels.append(
-            {
-                'id': None, 
-                'entity': example['text'][head_idx:tail_idx], 
-                'type': 'O', 
-                'begin': head_idx, 
-                'end': tail_idx,
-            }
-        )
+        substring = example['text'][head_idx:tail_idx]
+        subsequence = create_subsequence(substring, '-')
+        words.extend(subsequence[0])
+        labels.extend(subsequence[1])
     
-    return result
+    return words, labels
