01dcda7efe55df066650c6d2b3adbbbcc31ce9f5
swoook <kimseongwoo21@gmail.com>
2023-02-02 08:13:03 +0000

    🐳 chore(src.data.aihub_pfc): Implemented as a class
    - Need to handle marks or signs such as . ! ? , + - ... at the end of a sentence

--- src/data/aihub_pfc/transformation.py
+++ src/data/aihub_pfc/transformation.py
@@ -1,68 +1,68 @@
 from copy import deepcopy
 
 
-def create_positive_subsequence(substring, label):
-    words = substring.split()
-    labels = [''.join([label, '_B'])]
-    for idx in range(len(words) - 1): labels.append(''.join([label, '_I']))
-    return words, labels
+class Example:
+    def __init__(self):
+        self._word_sequence = list()
+        self._label_sequence = list()
+        self._negative = '-'
+        self._begin = '_B'
+        self._consecutive = '_I'
+        self.seq_len_threshold = 4
+        
+    def _create_positive_subsequence(self, substring, label):
+        words = substring.split()
+        labels = [''.join([label, self._begin])]
+        for idx in range(len(words) - 1): labels.append(''.join([label, self._consecutive]))
+        return words, labels
 
-def create_negative_subsequence(substring, label):
-    words = substring.split()
-    labels = ['-' for _ in range(len(words))]
-    return words, labels
-
-def create_subsequence(substring, label):
-    if label == '-': return create_negative_subsequence(substring, label)
-    else: return create_positive_subsequence(substring, label)
-
-def create_word_label_sequence(example):
-    words = list()
-    labels = list()
-    
-    if not example['NE']:
-        subsequence = create_subsequence(example['text'], '-')
-        words.extend(subsequence[0])
-        labels.extend(subsequence[1])
+    def _create_negative_subsequence(self, substring, label):
+        words = substring.split()
+        labels = [self._negative for _ in range(len(words))]
         return words, labels
     
-    entities_sorted = sorted(example['NE'], key=lambda item: item['begin'])
+    def _extend_subsequence(self, subsequence):
+        self._word_sequence.extend(subsequence[0])
+        self._label_sequence.extend(subsequence[1])
     
-    if entities_sorted[0]['begin'] > 0:
-        tail_idx = entities_sorted[0]['begin']
-        substring = example['text'][:tail_idx]
-        subsequence = create_subsequence(substring, '-')
-        words.extend(subsequence[0])
-        labels.extend(subsequence[1])
+    def _create_subsequence(self, substring, label):
+        subsequence = self._create_negative_subsequence(substring, label) if label == self._negative else self._create_positive_subsequence(substring, label)
+        self._extend_subsequence(subsequence)
+    
+    def create_sequence(self, example):
+        if not example['NE']:
+            self._create_subsequence(example['text'], '-')
         
-    for idx, entity in enumerate(entities_sorted[:-1]):
-        head_idx = entity['begin']
-        tail_idx = entity['end']
-        substring = example['text'][head_idx:tail_idx]
-        subsequence = create_subsequence(substring, entity['type'])
-        words.extend(subsequence[0])
-        labels.extend(subsequence[1])
+        else:
+            entities_sorted = sorted(example['NE'], key=lambda item: item['begin'])
+
+            if entities_sorted[0]['begin'] > 0:
+                tail_idx = entities_sorted[0]['begin']
+                substring = example['text'][:tail_idx]
+                subsequence = self._create_subsequence(substring, '-')
+
+            for idx, entity in enumerate(entities_sorted[:-1]):
+                head_idx = entity['begin']
+                tail_idx = entity['end']
+                substring = example['text'][head_idx:tail_idx]
+                subsequence = self._create_subsequence(substring, entity['type'])
+
+                head_idx = entities_sorted[idx]['end']
+                tail_idx = entities_sorted[idx+1]['begin']
+                substring = example['text'][head_idx:tail_idx]
+                subsequence = self._create_subsequence(substring, '-')
+
+            head_idx = entities_sorted[-1]['begin']
+            tail_idx = entities_sorted[-1]['end']
+            substring = example['text'][head_idx:tail_idx]
+            subsequence = self._create_subsequence(substring, entities_sorted[-1]['type'])
+
+            if entities_sorted[-1]['end'] < len(example['text']):
+                head_idx = entities_sorted[-1]['end']
+                tail_idx = len(example['text'])
+                substring = example['text'][head_idx:tail_idx]
+                subsequence = self._create_subsequence(substring, '-')
         
-        head_idx = entities_sorted[idx]['end']
-        tail_idx = entities_sorted[idx+1]['begin']
-        substring = example['text'][head_idx:tail_idx]
-        subsequence = create_subsequence(substring, '-')
-        words.extend(subsequence[0])
-        labels.extend(subsequence[1])
-    
-    head_idx = entities_sorted[-1]['begin']
-    tail_idx = entities_sorted[-1]['end']
-    substring = example['text'][head_idx:tail_idx]
-    subsequence = create_subsequence(substring, entities_sorted[-1]['type'])
-    words.extend(subsequence[0])
-    labels.extend(subsequence[1])
-    
-    if entities_sorted[-1]['end'] < len(example['text']):
-        head_idx = entities_sorted[-1]['end']
-        tail_idx = len(example['text'])
-        substring = example['text'][head_idx:tail_idx]
-        subsequence = create_subsequence(substring, '-')
-        words.extend(subsequence[0])
-        labels.extend(subsequence[1])
-    
-    return words, labels
+        if not len(self._label_sequence) >= self.seq_len_threshold:
+            return None
+        return self._word_sequence, self._label_sequence
