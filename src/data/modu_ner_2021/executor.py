86569e28aaf13a70e9c95ee934aaf75ef5f5ff83
swoook <kimseongwoo21@gmail.com>
2023-03-14 09:21:48 +0000

    🐳 chore(src.data.modu_ner_2021): Changed to merge into single `.tsv` file

--- src/data/modu_ner_2021/executor.py
+++ src/data/modu_ner_2021/executor.py
@@ -4,6 +4,7 @@ import os
 from dataset import Dataset
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
+import pandas as pd
 
 
 def parse_args():
@@ -25,14 +26,19 @@ def main(args):
             os.path.join(base_dir, filename) for filename in filenames if os.path.splitext(filename)[1] == '.json'
         ])
 
-    if not os.path.exists(args.dst_directory): os.makedirs(args.dst_directory)
-
+    if not os.path.exists(args.dst_directory): 
+        os.makedirs(args.dst_directory)
+    elif os.path.exists(args.dst_directory) and os.listdir(os.path.exists(args.dst_directory)):
+        raise PermissionError('`dst_directory` must be empty.')
+    tmp_directory = os.path.join(args.dst_directory, 'tmp')
+    os.makedirs(tmp_directory)
+        
     if args.num_cpus == 1:
         dataset = Dataset()
         for entry in tqdm(entries):
             src_path = entry
             src_name = os.path.splitext(os.path.basename(entry))[0]
-            dst_path = os.path.join(args.dst_directory, ''.join([src_name, '.tsv']))
+            dst_path = os.path.join(tmp_directory, ''.join([src_name, '.tsv']))
             dataset.convert(
                     src_path=src_path, 
                     dst_path=dst_path
@@ -42,11 +48,23 @@ def main(args):
         def process_map_fn(entry):
             src_path = entry
             src_name = os.path.splitext(os.path.basename(entry))[0]
-            dst_path = os.path.join(args.dst_directory, ''.join([src_name, '.tsv']))
+            dst_path = os.path.join(tmp_directory, ''.join([src_name, '.tsv']))
             dataset = Dataset(src_path)
             dataset.convert(dst_path=dst_path)
 
         process_map(process_map_fn, entries, max_workers=args.num_cpus)
+    
+    results = pd.DataFrame(columns=['X','Y'])
+    for idx, entry in enumerate(os.listdir(tmp_directory)):
+        if not os.path.splitext(entry)[1] == '.tsv': continue
+        dataframe = pd.read_csv(
+            os.path.join(tmp_directory, entry), 
+            sep='\t',
+            names=['X','Y']
+        )
+        results = pd.concat([results, dataframe])
+    dst_path = os.path.join(args.dst_directory, 'dataset.tsv')
+    results.to_csv(dst_path, sep="\t")
     
     
 if __name__ == '__main__':
